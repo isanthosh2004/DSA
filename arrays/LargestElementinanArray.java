4ab547c6c52b523b3320688ced3e4a72232d69bf
Santhosh Kumar S P <112749784+isanthosh2004@users.noreply.github.com>
2025-05-10 10:45:37 +0530

    Update LargestElementinanArray.java

--- arrays/LargestElementinanArray.java
+++ arrays/LargestElementinanArray.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] n = {3, 5, 6, 8, 1, 0, 20};
+        int max = n[0];  // initialize with first element
+
+        for (int i = 1; i < n.length; i++) {
+            if (n[i] > max) {
+                max = n[i];
+            }
+        }
+
+        System.out.println("Maximum value is: " + max);
+    }
+}
 
