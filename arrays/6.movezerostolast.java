0a4f8df7c1bec3d3ca14ecda4f46007f2e97f23d
Santhosh Kumar S P <112749784+isanthosh2004@users.noreply.github.com>
2025-05-11 20:38:44 +0530

    Create 6.movezerostolast.java

---
+++ arrays/6.movezerostolast.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int b = 0 ; 
+        for(int i = 0 ; i<nums.length;i++){
+            if(nums[i] == 0){
+                b++;
+
+            }else if(b>0){
+                int temp = nums[i];
+                nums[i] =0;
+                nums[i-b] =temp;
+            }
+        }
+        
+    }
+}
+
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
