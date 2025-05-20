86380d174d505d138c743fbd7dff297585d3b20a
Santhosh Kumar S P <112749784+isanthosh2004@users.noreply.github.com>
2025-05-20 09:44:37 +0530

    In the binary search for lower bound, the condition if (arr[mid] < x) checks whether the middle element is strictly less than the target x. If it is, then the lower bound must lie to the right of mid, so we update low = mid + 1. Otherwise, if arr[mid] >= x, then mid could be a potential lower bound, but there might be an earlier occurrence, so we narrow the search to the left side by setting high = mid. This ensures we find the first index where the element is greater than or equal to x.
    In the binary search for lower bound, the condition if (arr[mid] < x) checks whether the middle element is strictly less than the target x. If it is, then the lower bound must lie to the right of mid, so we update low = mid + 1. Otherwise, if arr[mid] >= x, then mid could be a potential lower bound, but there might be an earlier occurrence, so we narrow the search to the left side by setting high = mid. This ensures we find the first index where the element is greater than or equal to x.

---
+++ binarysearch/2.lower_bound.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class tUf {
+
+    public static int lowerBound(int []arr, int n, int x) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= x) {
+                // lower bound found:
+                return i;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+}
+
+
